c69c06697d9fc9b1820476aebc2e7f477800a612
Gabriel Cesar <gcal1@live.com>
2016-08-14 02:55:33 -0300

    Added views to create/edit blog post inside the site

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
 #create your views here
 
 #page home
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts_ord_publ_date = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts':posts_ord_publ_date})
