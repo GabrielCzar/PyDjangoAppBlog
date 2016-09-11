7668addc0d1847b9a1bc340e92d9188ef4144b17
Gabriel Cesar <gcal1@live.com>
2016-09-11 16:03:10 -0300

    Atualizando urls

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .forms import PostForm
 
 #page home
 def post_list(request):
-	posts_ord_publ_date = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts_ord_publ_date = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts_ord_publ_date})
 
 #page detail of post
